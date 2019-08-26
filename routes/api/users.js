7f932d13bdd5798ca256cd27aa5544b1fb4e4db0
SmackSmash <danmoffat@gmail.com>
2019-08-26 15:40:06 +0100

    Added body parsing middleware and req.body validation for the users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/users
-// @desc    Test route
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('Users route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter an email of 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+    res.send('Users route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile/', require('./routes/api/profile'));
-app.use('/api/posts/', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 connectDB();
 
