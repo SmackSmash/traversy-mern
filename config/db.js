e408566af9bce8bc7b1446ac88c9b7661d8c5da7
SmackSmash <danmoffat@gmail.com>
2019-08-27 17:07:41 +0100

    Resolved mongoose deprecation warning

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const mongoURI = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, { useCreateIndex: true, useNewUrlParser: true });
+    await mongoose.connect(mongoURI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
