a6f63358a82f5c31fbba3c91c5511e62f54d5d30
SmackSmash <danmoffat@gmail.com>
2019-08-29 12:49:52 +0100

    Added post model

--- models/user.js
+++ models/user.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  avatar: {
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model(
+  'user',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  })
+);
 
 const validateSignUp = Joi.object().keys({
   name: Joi.string()
