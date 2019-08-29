7bc8b7e4b4850c75da99a80a3312f04d275bfc15
SmackSmash <danmoffat@gmail.com>
2019-08-29 16:09:49 +0100

    Added basic component setup for client

---
+++ client/src/app.root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/layout/navbar.component';
+import Landing from './components/layout/landing.component';
+import './app.root.css';
+
+const App = props => (
+  <>
+    <Navbar />
+    <Landing />
+  </>
+);
+
+export default App;
